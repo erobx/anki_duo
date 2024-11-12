8ccdcc247a01a20335cb115ee39dcd231ac74dc1
erobx <erob7856@gmail.com>
2024-11-11 21:51:13 -0500

    cleanup

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/erobx/anki_duo
 
 go 1.22.4
 
-require github.com/joho/godotenv v1.5.1 // indirect
+require (
+	github.com/erobx/golingo v0.0.0-20241111221612-edebfb440252 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+)
--- main.go
+++ main.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
 	"os/exec"
+	"strings"
 
 	"github.com/erobx/anki_duo/cmd"
+	"github.com/erobx/golingo/duolingo"
+	"github.com/joho/godotenv"
+)
+
+const (
+	baseUrl = "https://www.duolingo.com"
+	abbr    = "es"
 )
 
 type Cmdr struct {
@@ -14,34 +23,99 @@ type Cmdr struct {
 }
 
 func NewCmdr(args []string) Cmdr {
-	return Cmdr{
-		cmd: exec.Command("python3", args...),
-	}
+	return Cmdr{}
 }
 
 // go run . JWT
 func main() {
-	var jwt string
-	fmt.Print("Enter your JWT from Duolingo: ")
-	fmt.Scan(&jwt)
+	//var jwt string
+	//fmt.Print("Enter your JWT from Duolingo: ")
+	//fmt.Scan(&jwt)
 
-	args := []string{"scripts/__main__.py", jwt}
-	c := NewCmdr(args)
-	c.runDuo()
-	c.runAnki()
-}
-
-func (c *Cmdr) runDuo() {
-	c.cmd.Stdout = os.Stdout
-	c.cmd.Stderr = os.Stderr
-	err := c.cmd.Run()
-	if err != nil {
-		log.Fatal("Could not get vocab from Duolingo")
-	}
+	//duo := createConn(jwt)
+	//vocab := duo.GetVocab(jwt, abbr)
+	playTest()
 }
 
 func (c *Cmdr) runAnki() {
 	fmt.Println("Importing to Anki")
 	anki := cmd.NewAnki()
 	anki.ImportFile()
+}
+
+func createConn(token string) *duolingo.Duolingo {
+	err := godotenv.Load()
+	if err != nil {
+		os.Exit(1)
+	}
+
+	username := os.Getenv("DUO_USERNAME")
+	//token := os.Getenv("DUO_JWT")
+
+	duo := duolingo.NewDuolingo(username, token, baseUrl, abbr)
+	return duo
+}
+
+func dumpVocab(vocab []interface{}) {
+	data, err := json.Marshal(vocab)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = os.WriteFile("vocab.json", data, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func playTest() {
+	data, err := os.ReadFile("vocab.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	n := len(data) - 2
+	data = data[2:n]
+	words := extractWords(data)
+	_ = words
+
+	//for _, word := range words[:2] {
+	//	fmt.Printf("Text: %s, Translations: %v\n", word.Text, word.Translations)
+	//}
+}
+
+type Word struct {
+	Text         string   `json:"text"`
+	Translations []string `json:"translations"`
+}
+
+// have to parse data for each matching {} skipping ,
+
+func extractWords(data []byte) []Word {
+	content := strings.TrimSpace(string(data))
+	objects := strings.Split(content, "},{")
+	var words []Word
+
+	for i, obj := range objects {
+		obj = strings.TrimSpace(obj)
+		if i == 0 && strings.HasPrefix(obj, "{") {
+			obj = obj[1:]
+		}
+		if i == len(objects)-1 && strings.HasSuffix(obj, "}") {
+			obj = obj[:len(obj)-1]
+		}
+
+		// Wrap in braces for valid JSON
+		obj = fmt.Sprintf("{%s}", obj)
+
+		// Unmarshal the JSON object into a Word struct
+		var word Word
+		err := json.Unmarshal([]byte(obj), &word)
+		if err != nil {
+			log.Fatal(err)
+		}
+		words = append(words, word)
+	}
+
+	return words
 }
